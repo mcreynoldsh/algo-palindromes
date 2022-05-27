cfa3d169ee940a7334f2d1ad075fa0d0916b8977
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-27 11:44:39 -0500

    initial py solution

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,17 @@
+import re 
 def palindrome(word):
-    # Write code here
+    word = str(word).lower()
+    word_list=[]
+    
+    for letter in word:
+        if re.search("[a-z0-9]",letter) != None :
+            word_list.append(letter)
+   
+    rev_list= word_list.copy()
+    rev_list.reverse()
+    
+    if "".join(word_list) == "".join(rev_list):
+        return True
+    else:
+        return False    
+    
